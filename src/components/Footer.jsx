f2e95d04b8eea6983a0014db6092b74cd529197e
MALAPAKA DEEPAK <deepakmalapakadvns@gmail.com>
2025-09-08 19:52:14 +0530

    fixed the footer theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,21 @@
+import ThemeContext from "../context/ThemeContext";
+import { useContext } from "react";
 const Footer = () => {
+  const { isDark } = useContext(ThemeContext);
   return (
-    <footer className="bg-gray-900 text-gray-300 py-4 mt-10">
+    <footer
+      className={`${
+        isDark
+          ? "bg-[#161b22]/60 text-[#c9d1d9]"
+          : "bg-[#f6f8fa]/70 text-[#24292f]"
+      } py-4 mt-10`}
+    >
       <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-between">
-        
         {/* Left Side */}
         <p className="text-sm mb-2 sm:mb-0">
           © {new Date().getFullYear()} PlayList-Counter. All rights reserved.
         </p>
-        
+
         {/* Right Side */}
         <p className="text-sm">
           Built by{" "}
